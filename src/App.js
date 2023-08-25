bd1b819f337608a0b7a13655fd5919e7f94eac14
Taniya Maurya <112754475+taniyamaurya@users.noreply.github.com>
2023-08-25 22:05:34 +0530

    zomato

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+// ## Namaste React Course by Akshay Saini
+// Chapter 09 - Optimizing our App
 
-function App() {
+import React from "react";
+
+import Header from "./Components/Header";
+import Body from "./Components/Body";
+// import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Error from "./Components/Error";
+import Contact from "./Components/Contact";
+import Login from "./Components/Login";
+import RestaurantMenu from "./Components/RestaurantMenu";
+import Profile from "./Components/ProfileClass";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; // for routing our page import createBrowserRouter and RouterProvider for providing router & Outlet for children component for nested routing
+import Footer from "./Components/Footer";
+
+/* My Food App structure will look like this, 
+            1) Header
+                - Logo
+                - Nav Items(right side)
+                - Cart
+            2) Body
+                - Search bar
+                - Restaurants List
+                    - Restaurant card
+                        - Image
+                        - Name
+                        - Rating
+            3) Footer
+                - Links
+                - Copyrights
+       
+*/
+
+// AppLayout component to render: Header, Outlet(it contain children component like body, About, Restaurant Menu etc) and Footer Component
+export const AppLayout = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <Outlet />
+      <Footer />
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+// call createBrowserRouter for routing different pages
--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Error from "./Components/Error";
+import Body from "./Components/Body";
+import About from "./Components/About";
+import Profile from "./Components/ProfileClass";
+import Contact from "./Components/Contact";
+import RestaurantMenu from "./Components/RestaurantMenu";
+import Login from "./Components/Login";
+import ReactDOM from "react-dom/client";
+import { AppLayout } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const appRouter = createBrowserRouter([
+    {
+      path: "/", // show path for routing
+      element: <AppLayout />, // show component for particular path
+      errorElement: <Error />, // show error component for path is different
+      children: [
+        // show children component for routing
+        {
+          path: "/",
+          element: <Body />,
+        },
+        {
+          path: "about",
+          element: <About />,
+          children: [{ // nested routing
+            path: "profile",
+            // element: <Profile />,
+          }]
+        },
+        {
+          path: "contact",
+          element: <Contact />,
+        },
+        {
+          path: "restaurant/:resId",
+          element: <RestaurantMenu />,
+        },
+      ],
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+  ]);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<RouterProvider router={appRouter} />); // render RouterProvider and use router as props and pass value appRouter
